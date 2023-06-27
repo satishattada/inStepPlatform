c446529901c1b14e55faf3f895df6cc76a35c757
bhoomika.prakash <bhoomika.prakash@bp.com>
2023-06-27 21:02:47 +0530

    Initial commit

---
+++ src/main/java/com/infy/InStepPlatform/InStepPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.infy.InStepPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InStepPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InStepPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infy/InStepPlatform/InStepPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.InStepPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InStepPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
